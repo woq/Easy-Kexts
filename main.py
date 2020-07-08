f40e29de929c69077539078bd0351a0a33aa9316
woq <33190773+woq@users.noreply.github.com>
2020-07-08 13:54:17 +0800

    is oK! Lilu is work

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 import requests
+import os
+
+exp = "https://github.com/acidanthera/Lilu/releases/download/1.4.4/Lilu-1.4.4-RELEASE.zip"
 
 
-def get_latest_version(owerandprojectname):
-    url = "https://api.github.com/repos/" + owerandprojectname + "/releases/latest"
-    return requests.get(url).json()["tag_name"]
 
+def get_file(repo):
+    url = "https://api.github.com/repos/" + repo + "/releases/latest"
 
-print(get_latest_version("acidanthera/Lilu"))
+    version = requests.get(url).json()["tag_name"]
+    if repo == "acidanthera/Lilu":
+        filename = "Lilu-" + version + "-RELEASE.zip"
+        if os.path.isfile("./Lilu/" + filename):
+            print("Lilu is ok" + version)
+            exit()
+        file = requests.get("https://github.com/" + repo + "/releases/download/" + version +"/" + filename)
+        with open("./Lilu/"+filename, "wb") as code:
+            code.write(file.content)
+
+get_file("acidanthera/Lilu")
