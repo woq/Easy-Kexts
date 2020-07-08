86e69ab73c7fccef8bafb0fe78e0b38107212edf
woq <33190773+woq@users.noreply.github.com>
2020-07-08 14:06:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,15 +7,26 @@ exp = "https://github.com/acidanthera/Lilu/releases/download/1.4.4/Lilu-1.4.4-RE
 
 def get_file(repo):
     url = "https://api.github.com/repos/" + repo + "/releases/latest"
-
+    githuburl = "https://github.com/"
+    downloadpath = "/releases/download/"
+    fanxiegang = "/"
     version = requests.get(url).json()["tag_name"]
     if repo == "acidanthera/Lilu":
         filename = "Lilu-" + version + "-RELEASE.zip"
         if os.path.isfile("./Lilu/" + filename):
-            print("Lilu is ok" + version)
+            print("Lilu is ok!!! The Version = " + version)
             exit()
-        file = requests.get("https://github.com/" + repo + "/releases/download/" + version +"/" + filename)
+        file = requests.get(githuburl + repo + version + fanxiegang + filename)
         with open("./Lilu/"+filename, "wb") as code:
             code.write(file.content)
+    if repo == "acidanthera/Lilu":
+        filename = "Lilu-" + version + "-RELEASE.zip"
+        if os.path.isfile("./Lilu/" + filename):
+            print("Lilu is ok!!! The Version = " + version)
+            exit()
+        file = requests.get(githuburl + repo + version + fanxiegang + filename)
+        with open("./Lilu/" + filename, "wb") as code:
+            code.write(file.content)
+
 
 get_file("acidanthera/Lilu")
