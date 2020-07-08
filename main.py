5526827eea768c533aae7bb33bea88ad4b2120ef
woq <33190773+woq@users.noreply.github.com>
2020-07-08 12:56:13 +0800

    获取版本号完成

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 import requests
-print(requests.get("https://github.com/acidanthera/Lilu/releases/latest").json())
+
+
+def get_latest_version(owerandprojectname):
+    url = "https://api.github.com/repos/" + owerandprojectname + "/releases/latest"
+    return requests.get(url).json()["tag_name"]
+
+
+print(get_latest_version("acidanthera/Lilu"))
