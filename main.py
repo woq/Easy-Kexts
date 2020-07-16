17a130d726c2abdf3302d7625426714ddfdb0da5
woq <eveqvq@gmail.com>
2020-07-16 11:22:47 +0800

    Update main.py
    JSON 解析API
    1.最新Release版本号
    2.最新Release下载地址
    3.判断文件是否存在，如果不存在下载文件

--- main.py
+++ main.py
@@ -1,32 +1,26 @@
 import requests
 import os
-
-exp = "https://github.com/acidanthera/Lilu/releases/download/1.4.4/Lilu-1.4.4-RELEASE.zip"
-
+import time
 
 
 def get_file(repo):
+    # GET /repos/:owner/:repo/releases/tags/:tag
     url = "https://api.github.com/repos/" + repo + "/releases/latest"
-    githuburl = "https://github.com/"
-    downloadpath = "/releases/download/"
-    fanxiegang = "/"
-    version = requests.get(url).json()["tag_name"]
-    if repo == "acidanthera/Lilu":
-        filename = "Lilu-" + version + "-RELEASE.zip"
-        if os.path.isfile("./Lilu/" + filename):
-            print("Lilu is ok!!! The Version = " + version)
-        else:
-            file = requests.get(githuburl + repo + version + fanxiegang + filename)
-            with open("./Lilu/"+filename, "wb") as code:
-                code.write(file.content)
-    if repo == "acidanthera/OpenCorePkg":
-        filename = "OpenCore-" + version + "-RELEASE.zip"
-        if os.path.isfile("./OpenCore/" + filename):
-            print("OpenCore is ok!!! The Version = " + version)
-        else:
-            file = requests.get(githuburl + repo + version + fanxiegang + filename)
-            with open("./OpenCore/" + filename, "wb") as code:
-                code.write(file.content)
+    # 转换成JSON
+    json = requests.get(url).json()
+    # 获取Filename
+    filename = json["assets"][0]['name']
+    # 判断文件是否存在，避免重复下载
+    if os.path.isfile(filename):
+        print("文件已存在" + repo + "     " + json["tag_name"])
+    else:
+        file = requests.get(json["assets"][0]['browser_download_url'])
+        with open(filename, "wb") as code:
+            code.write(file.content)
+
+        print("下载完毕" + repo + "     " + json["tag_name"])
+    time.sleep(30)  # 避免过度频繁引起nuke
+    return json["tag_name"], filename
 
 
 get_file("acidanthera/Lilu")
