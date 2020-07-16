0a670e8c35b83eb0de2823371d9add967f085065
woq <eveqvq@gmail.com>
2020-07-16 13:24:17 +0800

    基本完成
    1.自动生成HTML文件，后续只需添加项目名即可

--- main.py
+++ main.py
@@ -1,9 +1,50 @@
+# -*- coding: utf-8 -*-
 import requests
 import os
 import time
 
+global head
+head = """
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Easy-Kexts</title>
+    <link rel="stylesheet" href="https://cdn.bootcdn.net/ajax/libs/bulma/0.9.0/css/bulma.min.css">
+</head>
+<body>
+<section class="section" name="Core">
+    <div class="container">
+        <div class="columns is-vcentered is-centered">
+            <table class="table is-bordered ">
+                <thead>
+                <tr>
+                    <th><abbr>类别</abbr></th>
+                    <th><abbr>项目名</abbr></th>
+                    <th><abbr>版本号</abbr></th>
+                    <th><abbr>更新时间</abbr></th>
+                    <th><abbr>更新说明</abbr></th>
+                    <th><abbr>下载连接</abbr></th>
+                </tr>
+                </thead>
+                <tfoot>
+"""
 
-def get_file(repo):
+foot = """
+                </tfoot>
+            </table>
+        </div>
+    </div>
+</section>
+</body>
+</html>
+"""
+suojin = "                "
+bigsuojin = "                   "
+
+
+def get_file(repo, sort):
     # GET /repos/:owner/:repo/releases/tags/:tag
     url = "https://api.github.com/repos/" + repo + "/releases/latest"
     # 转换成JSON
@@ -12,16 +53,20 @@ def get_file(repo):
     filename = json["assets"][0]['name']
     # 判断文件是否存在，避免重复下载
     if os.path.isfile(filename):
-        print("文件已存在" + repo + "     " + json["tag_name"])
+        print("文件已存在" + repo + "\r" + json["tag_name"])
     else:
         file = requests.get(json["assets"][0]['browser_download_url'])
         with open(filename, "wb") as code:
             code.write(file.content)
 
-        print("下载完毕" + repo + "     " + json["tag_name"])
-    time.sleep(30)  # 避免过度频繁引起nuke
-    return json["tag_name"], filename
+        print("下载完毕" + repo + "\r" + json["tag_name"])
+
+    return str((suojin + "<tr>") + ('<th><span class="tag is-primary is-light">'+sort +'</span></th>') + ('<th><a href="https://github.com/'+repo+'" target="_blank"><span class="tag is-primary">'+ repo+'</span></a></th>') +('<th><span class="tag is-info">'+ json["tag_name"]+'</span></th>')+('<th><span class="tag is-success">'+json["published_at"]+'</span></th>')+('<th><span class="tag is-warning is-light">'+json["html_url"] +'</span></th>')+('<th><a href="./'+ filename +'" target="_blank"><span class="tag is-link">下载</span></a></th>')+ ("\n" + suojin + "</tr>"))
 
 
-get_file("acidanthera/Lilu")
-get_file("acidanthera/OpenCorePkg")
+head = head + get_file("acidanthera/Lilu", "CORE")
+head = head + get_file("acidanthera/OpenCorePkg", "CORE")
+
+
+with open("index.html", "w") as f:
+    f.write(head+foot)
