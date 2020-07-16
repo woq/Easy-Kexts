e6a651a09ab1a3b4b4089d79aa6abcd150dd0ef1
woq <eveqvq@gmail.com>
2020-07-16 11:23:07 +0800

    Update index.php
    php计划终止，用Python编写

--- index.php
+++ index.php
@@ -45,3 +45,14 @@
 </section>
 </body>
 </html>
+<?php
+$context = stream_context_create(
+    array(
+        "http" => array(
+            "header" => "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"
+        )
+    )
+);
+$json = json_decode(file_get_contents('https://api.github.com/repos/acidanthera/Lilu/releases/latest',false,$context));
+echo $json->tag_name;
+?>
