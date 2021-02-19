fe33325df1241d0b452f6d4fc2eba646a490e2c2
WOQ <3xleve@gmail.com>
2021-02-20 05:54:24 +0800

    完成了基本框架
    jinja2模板 循环除附件外所有内容
    附件内容负数,需要内建for循环

--- y.py
+++ y.py
@@ -1,12 +1,18 @@
-import jinja2,time
+import jinja2,time,requests
 from jinja2 import  Environment,FileSystemLoader
 env = Environment(loader=FileSystemLoader('D:\Code\Easy-Kexts'))
 
 tpl = env.get_template('template.html')
 
-
+anything = requests.get('https://github.com/woq/Easy-Kexts/raw/master/2.json').json()
 # sort1;sort2;projectName;projectNamelink;versionTag;releaseDate;updateWay=release,manual,direct;fileName;
+
+print(len(anything['projects']))
+for i in anything['projects']:
+    print(i)
+    print(type(i))
+
 CST = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 with open('report.html', 'w', encoding='utf8') as fout:
-    render_content = tpl.render(time=CST)
+    render_content = tpl.render(time=CST,list=anything)
     fout.write(render_content)
