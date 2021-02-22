ca62dc69df4be93ac6413f4965d316cb6cf4d3d1
WOQ <3xleve@gmail.com>
2021-02-22 13:22:23 +0800

    完成文件模块
    1.读取本地json
    2.如果更新方式为release,重写下载链接,项目链接,版本号,更新时间
    3.写出json

--- files.py
+++ files.py
@@ -1,16 +1,38 @@
 # -*- coding: utf-8 -*-
-import requests, os.path
+import requests, os.path, json, time
 
-# 读取本地JSON
-# 校验更新方式
-# 拉取最新信息,如果文件名不存在开始下载
 # 下载完成后,重写版本信息,发布时间,文件名,以及文件链接
+# 读取本地JSON
+with open('new.json', 'r') as load_f:
+    jsonData = json.load(load_f)
 
+i = 0
+for value in jsonData:
+    # 校验更新方式
+    if jsonData[i]['updateWay'] == 'release':
+        # GET /repos/:owner/:repo/releases/ api参考 https://developer.github.com/v3/repos/releases/
+        url = url = "https://api.github.com/repos/" + jsonData[i]['projectName'] + "/releases"
+        api = requests.get(url).json()
+        # 获取Filename 下载多个文件并准备HTML
+        assets = len(api[0]["assets"])
+        while assets >= 1:
+            assets = assets - 1
+            filename = api[0]["assets"][assets]['name']
+            if os.path.isfile(filename):
+                print("File exists File name is    " + filename)
+            else:
+                file = requests.get(api[0]["assets"][assets]['browser_download_url'])
+                with open(filename, "wb") as code:
+                    code.write(file.content)
+                print("Download Finished File Name is    " + filename)
+            jsonData[i]['files'] = '<a href="https://cdn.jsdelivr.net/gh/woq/Hackintosh-Resources/' + filename \
+                                   + '"target="_blank"><span class="tag is-link">' + filename + '</span></a>'
+            jsonData[i]['projectLink'] = api[0]['url']
+            jsonData[i]['versionTag'] = api[0]['tag_name']
+            jsonData[i]['releaseDate'] = api[0]['published_at']
+        time.sleep(3)
+    i += 1
 
-
-
-
-
-
-
-
+with open('new.json','w',encoding='utf8') as jsonDone:
+    wtf = json.dumps(jsonData)
+    jsonDone.write(wtf)
--- index.py
+++ index.py
@@ -1,48 +1,12 @@
 # -*- coding: utf-8 -*-
-import requests, os.path, time
+import json,time,os.path
+from jinja2 import Environment, FileSystemLoader
 from git import Repo
 
-
-
-def githubrepo( sort1, sort2, owner_repo):
-    # GET /repos/:owner/:repo/releases/ api参考 https://developer.github.com/v3/repos/releases/
-    url = "https://api.github.com/repos/" + owner_repo + "/releases"
-    api = requests.get(url).json()
-
-    # 获取Filename 下载多个文件并准备HTML
-    assets = len(api[0]["assets"])
-    while assets >= 1:
-        assets = assets - 1
-        filename = api[0]["assets"][assets]['name']
-        if os.path.isfile(filename):
-            print("File exists File name is    " + filename)
-        else:
-            file = requests.get(api[0]["assets"][assets]['browser_download_url'])
-            with open(filename, "wb") as code:
-                code.write(file.content)
-            print("Download Finished File Name is    " + filename)
-        downloading = downloading + '<a href="https://cdn.jsdelivr.net/gh/woq/Hackintosh-Resources/' + filename + '"target="_blank"><span class="tag is-link">' + filename + '</span></a>'
-
-
-
-# 下载Github项目里最新的一个tag的所有附件 备注 如果附件数量为0 会出致命错误
-githubRepos = requests.get("https://raw.githubusercontent.com/woq/Easy-Kexts/master/main.json").json()
-x = len(githubRepos["list"])
-y = 0
-while y < x:
-    head = head + githubrepo(githubRepos["list"][y]["sort1"], githubRepos["list"][y]["sort2"], githubRepos["list"][y]["repo"])
-    time.sleep(3)
-    y = y + 1
-
-
-import time, requests
-from jinja2 import Environment, FileSystemLoader
-
-env = Environment(loader=FileSystemLoader('D:\Code\Easy-Kexts'))
-
+env = Environment(loader=FileSystemLoader('./'))
 tpl = env.get_template('template.html')
 
-anything = requests.get('https://github.com/woq/Easy-Kexts/raw/master/test3.json').json()
+anything = json.load()
 # sort1;sort2;projectName;projectNamelink;versionTag;releaseDate;updateWay=release,manual,direct;fileName;
 
 
@@ -51,11 +15,6 @@ with open('index.html', 'w', encoding='utf8') as output:
     render_content = tpl.render(time=CST, list=anything)
     output.write(render_content)
 
-
-
-head = head+manual('Ethernet','Realtek','LucyRTL8125Ethernet','https://github.com/Mieze/LucyRTL8125Ethernet','1.0.0','unknow','手动 + CDN','https://cdn.jsdelivr.net/gh/woq/Hackintosh-Resources/LucyRTL8125Ethernet-V1.0.0.zip','LucyRTL8125Ethernet-V1.0.0.zip')
-head = head+manual('Ethernet','Atheros','AtherosE2200Ethernet','https://github.com/Mieze/AtherosE2200Ethernet','2.3.3','unknow','手动 + CDN','https://cdn.jsdelivr.net/gh/woq/Hackintosh-Resources/AtherosE2200Ethernet-V2.3.3.zip','AtherosE2200Ethernet-V2.3.3.zip')
-head = head+manual('CORE','EFI/OC/Drivers/','HfsPlus.efi','https://github.com/acidanthera/OcBinaryData/blob/master/Drivers/HfsPlus.efi','unknown','unknown','直连 + CDN','https://cdn.jsdelivr.net/gh/acidanthera/OcBinaryData/Drivers/HfsPlus.efi','HfsPlus.efi')
 
 
 # GitPython
