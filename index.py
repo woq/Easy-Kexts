613ba98f00ce979464f28094c951e1ba56450c92
WOQ <3xleve@gmail.com>
2021-02-22 13:28:28 +0800

    网页部分完成

--- index.py
+++ index.py
@@ -1,23 +1,24 @@
 # -*- coding: utf-8 -*-
 import json,time,os.path
 from jinja2 import Environment, FileSystemLoader
-from git import Repo
+
 
 env = Environment(loader=FileSystemLoader('./'))
 tpl = env.get_template('template.html')
 
-anything = json.load()
-# sort1;sort2;projectName;projectNamelink;versionTag;releaseDate;updateWay=release,manual,direct;fileName;
+with open('new.json', 'r') as load_f:
+    jsonData = json.load(load_f)
 
 
 CST = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 with open('index.html', 'w', encoding='utf8') as output:
-    render_content = tpl.render(time=CST, list=anything)
+    render_content = tpl.render(time=CST, list=jsonData)
     output.write(render_content)
 
 
-
 # GitPython
+"""
+from git import Repo
 dirfile = os.path.abspath('')
 repo = Repo(dirfile)
 g = repo.git
@@ -25,3 +26,4 @@ g.add("--all")
 g.commit("-m auto update")
 g.push()
 print("Successful push!")
+"""
